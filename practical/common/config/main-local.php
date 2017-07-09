f4702706f9bb91bc81cd5909076a0ad79041151a
ravialmt1 <ravialmt1@users.noreply.github.com>
2017-07-09 10:34:39 +0530

    dsn changed

---
+++ practical/common/config/main-local.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=mishika.c2fjpgtysnhg.us-west-2.rds.amazonaws.com:3306;dbname=newschema',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+'class' => 'yii\swiftmailer\Mailer',
+'useFileTransport' => false,
+'transport' => [
+'class' => 'Swift_SmtpTransport',
+'host' => 'smtp.gmail.com',
+'username' => 'ravialmt2@gmail.com',
+'password' => 'boxer123',
+'port' => '587',
+'encryption' => 'tsl',
+],
+],
+    ],
+];
